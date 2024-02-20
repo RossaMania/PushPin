c8ae6e480d7368cab98593f65b4d3c590a9ff121
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-20 16:02:33 -0600

    Add date_visited column to places table with null constraint and default value

--- db/migrate/20240219021605_add_date_visited_to_places.rb
+++ db/migrate/20240219021605_add_date_visited_to_places.rb
@@ -1,22 +1,14 @@
 class AddDateVisitedToPlaces < ActiveRecord::Migration[7.1]
   def up
-    # Add the column without the null constraint
-    add_column :places, :date_visited, :timestamp
+    # Add the column with the null constraint and default value
+    add_column :places, :date_visited, :timestamp, null: false, default: -> { 'CURRENT_DATE' }
 
-    # Update existing rows to set a default value or a meaningful value for date_visited
+    # Update existing rows to set a meaningful value for date_visited
     execute("UPDATE places SET date_visited = NOW() WHERE date_visited IS NULL")
-
-    # Add the null constraint now that existing rows are updated
-    change_column :places, :date_visited, :timestamp, null: false
   end
 
   def down
     # If you need to rollback the migration, reverse the changes
-    change_column :places, :date_visited, :timestamp, null: true
-
-    # Remove the default value set during the 'up' migration
-    execute("UPDATE places SET date_visited = NULL WHERE date_visited IS NOT NULL")
-
     remove_column :places, :date_visited
   end
 end
