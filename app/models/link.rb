8d08b4d073e1cadb8c0a407f0f755446238e35c4
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-27 14:03:35 -0600

    Update validations to allow blank values in link model for link url and link name.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
-  validates :url, allow_nil: true
-  validates :name, allow_nil: true
+  validates :url, allow_blank: true
+  validates :name, allow_blank: true
 end
