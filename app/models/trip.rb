36d1822feea1d88f58d4ee7af9c57d329620ddd2
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-27 14:36:35 -0600

    Remove dummy_presence_validation validation from Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,4 @@ class Trip < ApplicationRecord
   has_many :places,
     dependent: :destroy
   has_many :links, as: :linkable, dependent: :destroy
-  validates :dummy_presence_validation, presence: true
 end
