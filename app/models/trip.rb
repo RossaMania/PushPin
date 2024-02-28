17fc3fef9b7a2d33e17a3d2c8d435318315e9b99
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-28 14:43:13 -0600

    Remove accepts_nested_attributes_for for places in Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,5 @@ class Trip < ApplicationRecord
   has_many :places, dependent: :destroy
   has_many :links, as: :linkable, dependent: :destroy
 
-  accepts_nested_attributes_for :places, allow_destroy: true
   accepts_nested_attributes_for :links, allow_destroy: true
 end
