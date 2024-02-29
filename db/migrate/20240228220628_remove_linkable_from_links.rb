f3a54d9d33bd74497daf4d367ee68338f1613a52
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-29 12:52:34 -0600

    Update ActiveRecord::Migration version in RemoveLinkableFromLinks migration

--- db/migrate/20240228220628_remove_linkable_from_links.rb
+++ db/migrate/20240228220628_remove_linkable_from_links.rb
@@ -1,6 +1,9 @@
-class RemoveLinkableFromLinks < ActiveRecord::Migration[7.1]
+class RemoveLinkableFromLinks < ActiveRecord::Migration[6.1]
   def change
-    remove_column :links, :linkable_type, :string
-    remove_column :links, :linkable_id, :string
+    change_table :links, bulk: true do |t|
+      t.remove :linkable_type
+      t.remove :linkable_id
+      # Add other remove_column or add_column calls here if needed
+    end
   end
 end
