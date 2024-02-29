847c9698ede3be4d5174b9a6100cc0d17c7277ac
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-29 12:59:36 -0600

    Remove linkable columns from links table

--- db/migrate/20240228220628_remove_linkable_from_links.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLinkableFromLinks < ActiveRecord::Migration[7.1]
-  def change
-    change_table :links, bulk: true do |t|
-      t.remove :linkable_type
-      t.remove :linkable_id
-      # Add other remove_column or add_column calls here if needed
-    end
-  end
-end
--- db/migrate/20240228220948_add_linkable_to_links.rb
+++
@@ -1,5 +0,0 @@
-class AddLinkableToLinks < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :links, :linkable, polymorphic: true, null: false
-  end
-end
